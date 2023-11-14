a15c5ff481366de1d9e683c084fbfffbdc27c08d
Addison Migash <tuo20482@temple.edu>
2023-11-14 14:29:25 -0500

    Removed unnecessary imports, implemented saving with saveComic and loadComic. Changed formatting

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.example.networkapp
 
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
-
 
 private const val COMIC_TITLE_KEY = "comic_title"
 private const val COMIC_DESCRIPTION_KEY = "comic_description"
@@ -47,15 +46,16 @@ class MainActivity : AppCompatActivity() {
         loadComic()
     }
 
-    private fun downloadComic (comicId: String) {
-        val url = "https://xkcd.com/$comicId/info.0.json"
-        requestQueue.add (
-            JsonObjectRequest(url, {showComic(it)}, {
-            })
+    private fun downloadComic(comicId: String) {
+        requestQueue.add(
+            JsonObjectRequest(
+                "https://xkcd.com/$comicId/info.0.json",
+                { showComic(it) },
+                {})
         )
     }
 
-    private fun showComic (comicObject: JSONObject) {
+    private fun showComic(comicObject: JSONObject) {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
@@ -76,7 +76,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadComic() {
-        with (preferences) {
+        with(preferences) {
             titleTextView.text = getString(COMIC_TITLE_KEY, "Title")
             descriptionTextView.text = getString(COMIC_DESCRIPTION_KEY, "Description")
             getString(COMIC_IMAGE_URL_KEY, null)?.let {
